43bc3519d53d808cdf4781d4506dd7dbc3c4ddc7
Francisco Juarez <francisco.ipn94@yahoo.com>
2017-09-11 12:20:49 -0500

    Update holaMundo.c

--- holaMundo.c
+++ holaMundo.c
@@ -3,8 +3,8 @@
 int main()
 {
     printf("Hola mundo");
-	printf("Hola Carlos soy Manuel");
-	printf("Hola Carlos, Manuel soy Francisco");
+    printf("Hola Carlos soy Manuel");
+    printf("Hola Carlos, Manuel soy Francisco");
     printf("Prueba de git flow");
 	return 0;
 }
